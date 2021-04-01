f9cdf5897cc03ba45f9e8ecc38106fd282f78543
Vishwa Mittar <67235733+vishwasluhana@users.noreply.github.com>
2021-04-01 11:45:50 +0500

    Update check_duplicate.py

--- check_duplicate.py
+++ check_duplicate.py
@@ -5,13 +5,14 @@ for word in words.read().split("\""):
     if len(word) == 5:
         if word in seen:
             if word not in dups:
-                print(word)
                 dups.add(word)
         else:
             seen.add(word)
     else: continue
-
+    
 if len(dups) >= 1:
 	print("Duplicates found: ", len(dups))
+	for word in dups:
+		print(word)
 else:
 	print("No duplicates found")
